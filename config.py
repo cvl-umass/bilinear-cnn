639d467c1fa6586b9b354f88acf5baa54c63480d
TsungYu Lin <tsungyulin@cs.umass.edu>
2019-04-15 17:49:13 -0400

    copying dataset to local

--- config.py
+++ config.py
@@ -1,6 +1,67 @@
+import socket
+import copy
+from shutil import copytree
+import os
+import time
+
+
 dset_root = {}
 dset_root['cub'] = '/scratch1/tsungyulin/dataset/cub'
 dset_root['cars'] = '/scratch1/tsungyulin/dataset/cars'
 dset_root['aircrafts'] = '/scratch1/tsungyulin/dataset/fgvc-aircraft-2013b'
-dset_root['inat'] = '/scratch1/tsungyulin/dataset/inat_2018_448'
+dset_root['inat'] = '/scratch1/tsungyulin/dataset/inat_2018_224'
+
+test_code = False 
+
+
+if 'node' in socket.gethostname() or test_code:
+    nfs_dset = copy.deepcopy(dset_root)
+    if test_code:
+        local_path = os.path.join(os.getenv("HOME"), 'my_local_test')
+    else:
+        local_path = '/local/image_datasets'
+    if not os.path.isdir(local_path):
+        os.makedirs(local_path)
+    for x in dset_root.items():
+        folder_name = os.path.basename(x[1])
+        dset_root[x[0]] = os.path.join(local_path, folder_name)
+        
+def wait_dataset_copy_finish(dataset):
+    flag_file = os.path.join(dset_root[dataset] + '_flag',
+                            'flag_ready.txt')
+    while True:
+        with open(flag_file, 'r') as f:
+            status = f.readline()
+        if status == 'True':
+            break
+        time.sleep(20)
+
+
+def gypsum_setup_dataset(dataset):
+    if not os.path.isdir(dset_root[dataset]):
+        if os.path.isdir(os.path.join(dset_root[dataset] + '_flag')):
+            wait_dataset_copy_finish(dataset)
+        else:
+            gypsum_copy_data_to_local(dataset)
+    else:
+        wait_dataset_copy_finish(dataset)
+
+
+def gypsum_copy_data_to_local(dataset):
+    flag_file = os.path.join(dset_root[dataset] + '_flag', 'flag_ready.txt')
+
+    os.makedirs(dset_root[dataset] + '_flag')
+    with open(flag_file, 'w') as f:
+        f.write('False')
+    if test_code:
+        import pdb
+        pdb.set_trace()
+        pass
+    copytree(nfs_dset[dataset], dset_root[dataset])
+
+    if test_code:
+        pdb.set_trace()
+        pass
+    with open(flag_file, 'w') as f:
+        f.write('True')
 
